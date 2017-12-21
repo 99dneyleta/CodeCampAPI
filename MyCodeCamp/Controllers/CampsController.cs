9027c6d253fa5281de7fb31f8eccdda517897c33
99dneyleta <programmerversusgod@gmail.com>
2017-12-21 19:33:02 +0200

    added (get) api/camps

---
+++ MyCodeCamp/Controllers/CampsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MyCodeCamp.Controllers
+{
+    [Route("api/camps")]
+    public class CampsController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Get()
+        {
+            return Ok(new {Name = "Shawn", FavoriteColor = "Blue"});
+        }
+    }
+}
